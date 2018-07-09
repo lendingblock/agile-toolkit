96dbac4517d2bf4406c8227cecce8e9ac1bdb97a
Luca Sbardella <luca@quantmind.com>
2018-07-09 17:27:48 +0100

    manager

--- agiletoolkit/repo.py
+++ agiletoolkit/repo.py
@@ -5,18 +5,10 @@ from typing import Dict
 from dataclasses import dataclass
 from urllib.parse import urlparse
 
-import yaml
-
-from jinja2 import Template
-
+from .manager import Manager
 from .api import GithubApi, GithubException
 from .slack import SlackIntegration
 from . import utils
-
-
-NAMESPACES = {
-    'local', 'development', 'dev', 'stage', 'prod', 'sandbox', 'production'
-}
 
 
 @dataclass
@@ -25,21 +17,11 @@ class Branches:
     stage: str = 'deploy'
 
 
-class RepoManager:
-
-    def __init__(self, config=None, path=None, yes=False, namespace=None):
-        self.config = config or {}
-        self.path = path or os.getcwd()
-        self.deploy_path = os.path.join(self.path, 'deploy')
-        self.yes = yes
-        self.message_brokers = []
-        self.namespace = namespace
-        self.info = utils.gitrepo(self.path)
-        SlackIntegration.add(self)
-        self.init()
+class RepoManager(Manager):
 
     def init(self):
-        pass
+        self.info = utils.gitrepo(self.path)
+        SlackIntegration.add(self)
 
     def software_version(self):
         """Software version
@@ -141,44 +123,3 @@ class RepoManager:
         bits = path.split('.')
         bits.pop()
         return self.github().repo('.'.join(bits))
-
-    def get(self, name: str, DataClass: type) -> object:
-        cfg = self.config.get(name) or {}
-        return DataClass(**cfg)
-
-    def load_data(self, *paths: str) -> Dict:
-        filename = self.filename(*paths)
-        if not os.path.isfile(filename):
-            raise utils.CommandError('%s file missing' % filename)
-        with open(filename, 'r') as fp:
-            data = yaml.load(fp) or {}
-        data_namespace = data.pop(self.namespace, None)
-        for namespace in NAMESPACES:
-            data.pop(namespace, None)
-        if data_namespace:
-            data.update(data_namespace)
-        data['namespace'] = self.namespace
-        return data
-
-    def manifest(self, values, *paths, filename=None):
-        """Load a manifest file and apply template values
-        """
-        filename = filename or self.filename(*paths)
-        with open(filename, 'r') as fp:
-            template = Template(fp.read())
-        return yaml.load(template.render(values))
-
-    def filename(self, *paths):
-        if not os.path.isdir(self.deploy_path):
-            raise utils.CommandError(
-                "Path '%s' not available" % self.deploy_path
-            )
-        return os.path.join(self.deploy_path, *paths)
-
-    def copy_env(self, *args, **kw):
-        env = os.environ.copy()
-        env.update(*args, **kw)
-        return env
-
-    def wait(self, coro):
-        return asyncio.get_event_loop().run_until_complete(coro)
