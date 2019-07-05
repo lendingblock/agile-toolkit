6935132c9376923c75c44cd5abc468bfce36ef3f
Luca Sbardella <luca@quantmind.com>
2019-07-05 08:22:10 +0100

    Fix slack client

--- agiletoolkit/manager.py
+++ agiletoolkit/manager.py
@@ -1,13 +1,11 @@
-import os
 import asyncio
+import os
 from typing import Dict
 
 import yaml
-
 from jinja2 import Template
 
 from . import utils
-
 
 NAMESPACES = {
     'local', 'development', 'dev', 'stage', 'prod', 'sandbox', 'production'
@@ -24,19 +22,28 @@ class Manager:
         self.namespace = namespace
         self.init()
 
-    def init(self):
+    def init(self) -> None:
         pass
 
     def get(self, name: str, DataClass: type) -> object:
         cfg = self.config.get(name) or {}
         return DataClass(**cfg)
 
+    def message(self, msg: str) -> None:
+        """Send a message to third party applications
+        """
+        for broker in self.message_brokers:
+            try:
+                broker(msg)
+            except Exception as exc:
+                utils.error(exc)
+
     def load_data(self, *paths: str, filename: str = None) -> Dict:
         filename = filename or self.filename(*paths)
         if not os.path.isfile(filename):
             raise utils.CommandError('%s file missing' % filename)
         with open(filename, 'r') as fp:
-            data = yaml.load(fp) or {}
+            data = yaml.load(fp, Loader=yaml.FullLoader) or {}
         data_namespace = data.pop(self.namespace, None)
         for namespace in NAMESPACES:
             data.pop(namespace, None)
@@ -51,7 +58,7 @@ class Manager:
         filename = filename or self.filename(*paths)
         with open(filename, 'r') as fp:
             template = Template(fp.read())
-        return yaml.load(template.render(values))
+        return yaml.load(template.render(values), Loader=yaml.FullLoader)
 
     def filename(self, *paths) -> str:
         return os.path.join(self.path, *paths)
